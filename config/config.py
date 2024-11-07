7de0f926fb8158d3c4c1e4752c5c9a04c12bcaba
huhgtzumo <zimo0124zp@gmail.com>
2024-11-08 03:53:20 +0800

    Add: config files for deployment

---
+++ config/config.py
@@ -0,0 +1,32 @@
+# Telegram Bot Token
+BOT_TOKEN = "7589935826:AAGIiHyKtPAo41j9ttRJrz4bBI9mCErzgiE"  # 替換為你的實際 bot token
+
+# 群組配置
+GROUPS = {
+    "group1": {
+        "id": -4564654363,  # 替換為實際的群組 ID
+        "name": "GN測試群"
+    },
+    "group2": {
+        "id": -4541534274,  # 請替換為第二個群組的實際 ID
+        "name": "土狗群"
+    }
+}
+
+# 頻道配置
+CHANNELS = {
+    "channel1": {
+        "id": -1002249802494,  # 請替換為頻道的實際 ID
+        "name": "TON Defense ANN"
+    },
+    "channel2": {
+        "id": -1002363788102,
+        "name": "測試頻道"
+    }
+}
+
+# 超級管理員 ID 列表
+SUPER_ADMIN_IDS = [1307419995]  # 替換為你的 Telegram ID
+
+# 普通管理員 ID 列表
+ADMIN_USER_IDS = [1854400751, 458951698]  # 請替換 "新管理員ID" 為實際的 ID
